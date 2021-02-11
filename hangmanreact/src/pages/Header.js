b108e2828cd72b23aa2dcdec2a457e8f0e9437ed
AbdullahAleem <abdullahaleem2102@gmail.com>
2021-02-11 19:27:27 +0500

    clean code

--- hangmanreact/src/pages/Header.js
+++ hangmanreact/src/pages/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Header = () => {
+    //---------Working-------
+    //Just a simple header function
     return (
         <div>
            <h1>Hangman</h1>
--- hangmanreact/src/pages/Notification.js
+++ hangmanreact/src/pages/Notification.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Notification = ({showNotification}) => {
+  //     --------Working--------
+  //it shows the notication when someone 
   console.log('notication AND SHOULD I SHOW', showNotification)
     return (
         <div className={`notification-container ${showNotification?'show':''}`} >
